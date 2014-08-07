b7e34994de7d23c7e8180aae0a859b8181f21cb8
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2014-08-07 11:26:20 -0500

    updated to v0.1.1, cleaned up some unneeded code

--- content.js
+++ content.js
@@ -2,18 +2,16 @@
 $("td.default span.comhead").prepend('<a class="hn-fold">[-]</a> ');
 
 $(".hn-fold").click(function() {
-  console.log();
   // get width of this element
   var original_width = parseInt($("td:first img", $(this).closest("tr")).attr("width"));
   var orig_vis = $(this).closest(".default").find(".comment").css("display") === "none";
-  console.log("Comment width: " + original_width, " visibility: " + orig_vis);
+  // console.log("Comment width: " + original_width, " visibility: " + orig_vis);
 
   // find top-level tr and all siblings after it in the DOM
   var clicked_tr = $(this).closest("table").closest("tr");
   var clicked_siblings = $(" ~ tr", $(clicked_tr));
 
   var nodes_to_toggle = []
-  // var in_tree = true;
 
   // add original node
   nodes_to_toggle.push(clicked_tr);
@@ -23,18 +21,11 @@ $(".hn-fold").click(function() {
     var c_width = parseInt($("td:first img", $(this).find("table tr")).attr("width"));
     var c_vis = $(this).find("tr .comment").css("display") === "none";
 
-    // var comparison = c_width > original_width;
-    // console.log("original_width:" + original_width + ", c_width: " + c_width + ", c_width > original_width:" + (c_width > original_width) + ", comparison:" + comparison);
-
-    // if (in_tree && (c_width > original_width)) {
     if (c_width > original_width) {
-    // if (comparison) {
-      if (c_vis === orig_vis) {
-        nodes_to_toggle.push($(this));
-      }
+      if (c_vis === orig_vis) nodes_to_toggle.push($(this));
+
       console.log("sibling: " + c_width);
     } else {
-      // in_tree = false;
       console.log("NEW TREE GETTING OUT: current:" + original_width + " sibling: " + c_width);
       return false;
     }
@@ -42,19 +33,16 @@ $(".hn-fold").click(function() {
 
   console.log("comments to toggle: " + nodes_to_toggle.length);
 
+  // toggle all selected nodes
   $(nodes_to_toggle).each(function() {
     var comment = $(this).find("tr .comment");
     $(comment).toggle();
 
+    // update the fold link
     var fold_link = $(this).find(".hn-fold");
-    console.log("fold_link text: " + $(fold_link).text());
-
-    if ($(fold_link).text() === "[-]") {
-      console.log("MAKING PLUS");
+    if ($(fold_link).text() === "[-]")
       $(fold_link).html("[+]");
-    } else {
-      console.log("MAKING MINUS");
-      $(fold_link).html("[-]");
-    }
+    else
+      $(fold_link).html("[-]"); 
   });
 });
