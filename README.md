f47e599e5303d8d3608f401db9c06161b4cdc5ef
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2014-08-07 11:53:07 -0500

    updated to 0.1.2, made it so the extraneous reply links don't show up after code folding

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 TODO:
-- plus/minus sign switching
-- figure out why some replys aren't hiding
+- plus/minus sign switching ==> DONE
+- figure out why some replys aren't hiding ==> DONE
 - probably make it so when you hide a comment it hides everything like how RES does
 - probably refactor code, it looks kind of ugly
 - create icon for the extension
--- content.js
+++ content.js
@@ -5,12 +5,10 @@ $(".hn-fold").click(function() {
   // get width of this element
   var original_width = parseInt($("td:first img", $(this).closest("tr")).attr("width"));
   var orig_vis = $(this).closest(".default").find(".comment").css("display") === "none";
-  // console.log("Comment width: " + original_width, " visibility: " + orig_vis);
 
   // find top-level tr and all siblings after it in the DOM
   var clicked_tr = $(this).closest("table").closest("tr");
   var clicked_siblings = $(" ~ tr", $(clicked_tr));
-
   var nodes_to_toggle = []
 
   // add original node
@@ -23,15 +21,8 @@ $(".hn-fold").click(function() {
 
     if (c_width > original_width) {
       if (c_vis === orig_vis) nodes_to_toggle.push($(this));
-
-      console.log("sibling: " + c_width);
-    } else {
-      console.log("NEW TREE GETTING OUT: current:" + original_width + " sibling: " + c_width);
-      return false;
-    }
+    } else return false;
   });
-
-  console.log("comments to toggle: " + nodes_to_toggle.length);
 
   // toggle all selected nodes
   $(nodes_to_toggle).each(function() {
@@ -44,5 +35,8 @@ $(".hn-fold").click(function() {
       $(fold_link).html("[+]");
     else
       $(fold_link).html("[-]"); 
+
+    // hide any other extraneous sections in the comment
+    $(comment).nextAll().toggle();
   });
 });
