154aa28d2b097395c46a00d6bf8f3769b64fbfac
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2014-08-25 10:44:32 -0500

    0.1.3 version, for right now only single-collapse (or whatever you want to call it)

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ TODO:
 - figure out why some replys aren't hiding ==> DONE
 - probably make it so when you hide a comment it hides everything like how RES does
 - probably refactor code, it looks kind of ugly
-- create icon for the extension
--- content.js
+++ content.js
@@ -1,7 +1,4 @@
-// add in the button things
-$("td.default span.comhead").prepend('<a class="hn-fold">[-]</a> ');
-
-$(".hn-fold").click(function() {
+function fold_comments_one () {
   // get width of this element
   var original_width = parseInt($("td:first img", $(this).closest("tr")).attr("width"));
   var orig_vis = $(this).closest(".default").find(".comment").css("display") === "none";
@@ -43,4 +40,57 @@ $(".hn-fold").click(function() {
     // hide any other extraneous sections in the comment
     $(comment).nextAll().toggle();
   });
-});
+}
+
+
+// add in the button things
+$("td.default span.comhead").prepend('<a class="hn-fold">[-]</a> ');
+$(".hn-fold").click(fold_comments_one);
+
+
+
+
+// TODO: fix later
+// function fold_comments_full () {
+//   // get width of this element
+//   var original_width = parseInt( $("td:first img", $(this).closest("tr") ).attr("width") );
+//   var original_vis = $(this).closest(".default").find(".comment").css("display") === "none";
+//   console.log(original_width + " " + original_vis);
+
+//   // find top-level tr and all siblings after it in the DOM
+//   var clicked_tr = $(this).closest("table").closest("tr");
+//   var clicked_siblings = $(" ~ tr", $(clicked_tr));
+//   var num_siblings = 0;
+
+//   // fold clicked comment and update fold link
+//   var clicked_comment = $(clicked_tr).find("tr .comment");
+//   $(clicked_comment).toggle();
+//   var fold_link = $(clicked_tr).find(".hn-fold");
+//   if ($(fold_link).text() === "[-]") {
+//     $(fold_link).html("[+]");
+//     $(clicked_tr).find(".comhead").addClass("folded");
+//   } else {
+//     $(fold_link).html("[-]");
+//     $(clicked_tr).find(".comhead").removeClass("folded");
+//   }
+
+//   // toggle all children nodes
+//   $(clicked_siblings).each(function() {
+//     var comment_width = parseInt( $("td:first img", $(this).find("table tr") ).attr("width") );
+//     var comment_vis = ($(this).find("tr").css("display") === "none") ||
+//                       ($(this).find("tr .comment").css("display") === "none");
+
+//     // make sure it is a child node and that it is the same state
+//     if (comment_width > original_width) {
+//       if (comment_vis === original_vis) {
+//         $(this).toggle();
+//         num_siblings += 1;
+
+//         if $()
+//       }
+//     } else return false;
+//   });
+
+//   console.log("num siblings: " + num_siblings);
+// }
+
