05f8bb6025dd4054a7547321fa988e232126aba2
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2014-09-15 18:02:55 -0500

    started the rewrite

--- content.js
+++ content.js
@@ -1,51 +1,51 @@
-function fold_comments_one () {
-  // get width of this element
-  var original_width = parseInt($("td:first img", $(this).closest("tr")).attr("width"));
-  var orig_vis = $(this).closest(".default").find(".comment").css("display") === "none";
+// function fold_comments_one () {
+//   // get width of this element
+//   var original_width = parseInt($("td:first img", $(this).closest("tr")).attr("width"));
+//   var orig_vis = $(this).closest(".default").find(".comment").css("display") === "none";
 
-  // find top-level tr and all siblings after it in the DOM
-  var clicked_tr = $(this).closest("table").closest("tr");
-  var clicked_siblings = $(" ~ tr", $(clicked_tr));
-  var nodes_to_toggle = []
+//   // find top-level tr and all siblings after it in the DOM
+//   var clicked_tr = $(this).closest("table").closest("tr");
+//   var clicked_siblings = $(" ~ tr", $(clicked_tr));
+//   var nodes_to_toggle = []
 
-  // add original node
-  nodes_to_toggle.push(clicked_tr);
+//   // add original node
+//   nodes_to_toggle.push(clicked_tr);
 
-  // find all children nodes of the clicked node
-  $(clicked_siblings).each(function() {
-    var c_width = parseInt($("td:first img", $(this).find("table tr")).attr("width"));
-    var c_vis = $(this).find("tr .comment").css("display") === "none";
+//   // find all children nodes of the clicked node
+//   $(clicked_siblings).each(function() {
+//     var c_width = parseInt($("td:first img", $(this).find("table tr")).attr("width"));
+//     var c_vis = $(this).find("tr .comment").css("display") === "none";
 
-    // make sure it is a children and that it is the same state
-    if (c_width > original_width) {
-      if (c_vis === orig_vis) nodes_to_toggle.push($(this));
-    } else return false;
-  });
+//     // make sure it is a children and that it is the same state
+//     if (c_width > original_width) {
+//       if (c_vis === orig_vis) nodes_to_toggle.push($(this));
+//     } else return false;
+//   });
 
-  // toggle all selected nodes
-  $(nodes_to_toggle).each(function() {
-    var comment = $(this).find("tr .comment");
-    $(comment).toggle();
+//   // toggle all selected nodes
+//   $(nodes_to_toggle).each(function() {
+//     var comment = $(this).find("tr .comment");
+//     $(comment).toggle();
 
-    // update the fold link
-    var fold_link = $(this).find(".hn-collapse");
-    if ($(fold_link).text() === "[-]") {
-      $(fold_link).html("[+]");
-      $(this).find(".comhead").addClass("collapsed");
-    } else {
-      $(fold_link).html("[-]"); 
-      $(this).find(".comhead").removeClass("collapsed");
-    }
+//     // update the fold link
+//     var fold_link = $(this).find(".hn-collapse");
+//     if ($(fold_link).text() === "[-]") {
+//       $(fold_link).html("[+]");
+//       $(this).find(".comhead").addClass("collapsed");
+//     } else {
+//       $(fold_link).html("[-]"); 
+//       $(this).find(".comhead").removeClass("collapsed");
+//     }
 
-    // hide any other extraneous sections in the comment
-    $(comment).nextAll().toggle();
-  });
-}
+//     // hide any other extraneous sections in the comment
+//     $(comment).nextAll().toggle();
+//   });
+// }
 
 
-// add in the button things
-$("td.default span.comhead").prepend('<a class="hn-collapse">[-]</a> ');
-$(".hn-collapse").click(fold_comments_one);
+// // add in the button things
+// $("td.default span.comhead").prepend('<a class="hn-collapse">[-]</a> ');
+// $(".hn-collapse").click(fold_comments_one);
 
 
 
@@ -94,3 +94,38 @@ $(".hn-collapse").click(fold_comments_one);
 //   console.log("num siblings: " + num_siblings);
 // }
 
+
+
+
+
+
+
+function single_collapse () {
+  var clicked_link = $(this);
+
+  // info for clicked comment
+  var clicked_width = parseInt($("td:first img", clicked_link.closest("tr")).attr("width"));
+  var clicked_visibility = clicked_link.closest(".default").find(".comment").css("display") === "none";
+
+  // get clicked element and all other possible elements that may need changing
+  var clicked_tr = clicked_link.closest("table").closest("tr");
+  var possible_nodes = $(" ~ tr", $(clicked_tr)).toArray();
+  possible_nodes.unshift(clicked_tr);
+
+  $(possible_nodes).each(function(node_idx) {
+    var node = $(this);
+
+    // info for current node
+    var node_width = parseInt($("td:first img", node.find("table tr")).attr("width"));
+    var node_visibility = node.find("tr .comment").css("display") === "none";
+    // console.log(node_idx + " " + node_width + " " + node_visibility);
+
+    // make sure node is either a child node or the original node
+    // if ((node_width > clicked_width) || (node_idx == 0))
+  });
+
+  
+}
+
+$("td.default span.comhead").prepend('<a class="yahnes-collapse">[-]</a> ');
+$(".yahnes-collapse").click(single_collapse);
